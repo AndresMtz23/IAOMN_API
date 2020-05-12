32cd56669271c675fd35b6e700341cc1440ebe4d
Carlos Alberto Marún Rodríguez <carlos_marunro@outlook.com>
2020-05-12 18:19:19 -0500

    Api Test

--- IAOMN_API_REST/App_Start/WebApiConfig.cs
+++ IAOMN_API_REST/App_Start/WebApiConfig.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using System.Web.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
+
+
 namespace IAOMN_API_REST
 {
     public static class WebApiConfig
@@ -10,6 +16,16 @@ namespace IAOMN_API_REST
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+
+            jsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            settings.Formatting = Formatting.Indented;
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.EnableCors();
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
